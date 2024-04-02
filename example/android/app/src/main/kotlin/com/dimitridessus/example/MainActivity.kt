287c509897670c413d9d4ea90f26c01c7a6e05b6
Dimitri Dessus <dimitri.dessus@gmail.com>
2024-04-02 11:55:37 +0200

    🚚 Upgrade Android project

---
+++ example/android/app/src/main/kotlin/com/dimitridessus/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dimitridessus.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- example/android/app/src/main/kotlin/com/dimitridessus/gaimon_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.dimitridessus.gaimon_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
